7155bf7f8a2ab14d694b4e0334f4bf3600dc39f3
Chengjie Mao <minnows.lodge_0s@icloud.com>
2024-06-18 19:30:19 -0400

    Implement "Merge sorted arrays"

--- Sources/LeetCode/LeetCode.swift
+++ Sources/LeetCode/LeetCode.swift
@@ -1,2 +1,33 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+/// Merge sorted arrays.
+///
+/// You are given two integer arrays `nums1` and `nums2`, sorted in
+/// **non-decreasing order**, and two integers `m` and `n`, representing the
+/// number of elements in `nums1` and `nums2` respectively.
+///
+/// Merge `nums1` and `nums2` into a single array sorted in
+/// **non-decreasing order**.
+///
+/// The final sorted array should not be returned by the function, but instead
+/// be stored inside the array `nums1`. To accommodate this, `nums1` has a
+/// length of `m + n`, where the first `m` elements denote the elements that
+/// should be merged, and the last `n` elements are set to `0` and should be
+/// ignored. `nums2` has a length of `n`.
+func mergeSortedArrays(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+	var (i, j) = (m - 1, n - 1)
+	for k in nums1.indices.reversed() {
+		if j < 0 { break }
+		if i < 0 {
+			nums1[k] = nums2[j]
+			j -= 1
+			continue
+		}
+		let (n1, n2) = (nums1[i], nums2[j])
+		if n1 > n2 {
+			nums1[k] = n1
+			i -= 1
+		} else { // n1 <= n2
+			nums1[k] = n2
+			j -= 1
+		}
+	}
+}
--- Tests/LeetCodeTests/LeetCodeTests.swift
+++ Tests/LeetCodeTests/LeetCodeTests.swift
@@ -1,6 +1,31 @@
 import Testing
 @testable import LeetCode
 
-@Test func example() async throws {
-	// Write your test here and use APIs like `#expect(...)` to check expected conditions.
+extension [Int] {
+	func countExcludingTrailingZeros() -> Int {
+		if let lastNonZero = lastIndex(where: { $0 != 0 }) {
+			lastNonZero + 1
+		} else {
+			0
+		}
+	}
+}
+
+@Test("Merge sorted arrays", arguments: zip([
+	[[1,2,3,0,0,0], [2,5,6]],
+	[[1], []],
+	[[0], [1]]
+], [
+	[1,2,2,3,5,6],
+	[1],
+	[1]
+]))
+func testMergeSortedArrays(input: [[Int]], expected: [Int]) throws {
+	try #require(input.count == 2)
+	var nums1 = input[0]
+	let nums2 = input[1]
+	let (m, n) = (nums1.countExcludingTrailingZeros(), nums2.count)
+	try #require(m >= 0 && n >= 0 && m + n >= 1 && nums1.count == m + n)
+	mergeSortedArrays(&nums1, m, nums2, n)
+	#expect(nums1 == expected)
 }
