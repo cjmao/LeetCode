02f58e61347267972945c6197636aa719117b12d
Chengjie Mao <minnows.lodge_0s@icloud.com>
2024-07-19 18:35:31 -0400

    Implement "Minimum size subarray sum"

--- Sources/LeetCode/SlidingWindow.swift
+++ Sources/LeetCode/SlidingWindow.swift
@@ -4,5 +4,19 @@
 /// return _the **minimal length** of a subarray whose sum is greater than or
 /// equal to `target`_. If there is no such subarray, return `0` instead.
 func minSubArrayLen(_ target: Int, _ nums: [Int]) -> Int {
-	0
+	var minCount = nums.count + 1
+	var sum = 0
+	var i = 0
+
+	for (j, n) in nums.enumerated() {
+		sum += n
+
+		while sum >= target {
+			minCount = min(j - i + 1, minCount)
+			sum -= nums[i]
+			i += 1
+		}
+	}
+
+	return minCount > nums.count ? 0 : minCount
 }
--- Tests/LeetCodeTests/SlidingWindowTests.swift
+++ Tests/LeetCodeTests/SlidingWindowTests.swift
@@ -7,6 +7,8 @@ struct SlidingWindowTests {
 		TestCase(given: Pair(7, [2, 3, 1, 2, 4, 3]), expected: 2),
 		TestCase(given: Pair(4, [1, 4, 4]), expected: 1),
 		TestCase(given: Pair(11, [1, 1, 1, 1, 1, 1, 1, 1]), expected: 0),
+		TestCase(given: Pair(15, [5, 1, 3, 5, 10, 7, 4, 9, 2, 8]), expected: 2),
+		TestCase(given: Pair(5, [2, 3, 1, 1, 1, 1, 1]), expected: 2),
 	])
 	func testMinimumSizeSubarraySum(c: TestCase<Pair<Int, [Int]>, Int>) throws {
 		let ((target, nums), expected) = (c.given.values, c.expected)
