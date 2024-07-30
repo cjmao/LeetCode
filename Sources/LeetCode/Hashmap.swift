fe931b86fdd6c8f012877960923af64b9a41a2ec
Chengjie Mao <minnows.lodge_0s@icloud.com>
2024-07-30 11:51:53 -0400

    Add "Isomorphic strings"

--- Sources/LeetCode/Hashmap.swift
+++ Sources/LeetCode/Hashmap.swift
@@ -8,3 +8,17 @@
 func canConstruct(_ ransomNote: String, _ magazine: String) -> Bool {
 	false
 }
+
+/// Isomorphic strings
+///
+/// Given two strings `s` and `t`, *determine if they are isomorphic*.
+///
+/// Two strings `s` and `t` are isomorphic if the characters in `s` can be
+/// replaced to get `t`.
+///
+/// All occurrences of a character must be replaced with another character while
+/// preserving the order of characters. No two characters may map to the same
+/// character, but a character may map to itself.
+func isIsomorphic(_ s: String, _ t: String) -> Bool {
+	false
+}
--- Tests/LeetCodeTests/HashmapTests.swift
+++ Tests/LeetCodeTests/HashmapTests.swift
@@ -21,4 +21,19 @@ struct HashmapTests {
 
 		#expect(canConstruct(note, magazine) == expected)
 	}
+
+	@Test("Isomorphic strings", arguments: [
+		TestCase(given: Pair("egg", "add"), expected: true),
+		TestCase(given: Pair("foo", "bar"), expected: false),
+		TestCase(given: Pair("paper", "title"), expected: true),
+	])
+	func testIsIsomorphic(c: TestCase<Pair<String, String>, Bool>) throws {
+		let ((s, t), expected) = (c.given.values, c.expected)
+
+		try #require(!s.isEmpty && s.count == t.count)
+		try #require(s.allSatisfy { $0.isASCII })
+		try #require(t.allSatisfy { $0.isASCII })
+
+		#expect(isIsomorphic(s, t) == expected)
+	}
 }
