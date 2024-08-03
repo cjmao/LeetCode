8a1cd6ab1ec2f025103d9baede3cb4f1e83915d6
Chengjie Mao <minnows.lodge_0s@icloud.com>
2024-08-02 20:03:07 -0400

    Add "Simplify path"

--- Tests/LeetCodeTests/StackTests.swift
+++ Tests/LeetCodeTests/StackTests.swift
@@ -14,4 +14,24 @@ struct StackTests {
 		try #require(!s.isEmpty && s.allSatisfy { characters.contains($0) })
 		#expect(isValid(s) == expected)
 	}
+
+	@Test("Simplify path", arguments: [
+		TestCase(given: "/home/", expected: "/home"),
+		TestCase(given: "/home//foo/", expected: "/home/foo"),
+		TestCase(
+			given: "/home/user/Documents/../Pictures",
+			expected: "/home/user/Pictures"
+		),
+		TestCase(given: "/../", expected: "/"),
+		TestCase(given: "/.../a/../b/c/../d/./", expected: "/.../b/d"),
+	])
+	func testSimplifyPath(c: TestCase<String, String>) throws {
+		let (path, expected) = (c.given, c.expected)
+
+		try #require(!path.isEmpty && path.wholeMatch(
+			of: /([[:alnum:]]|\.|\/|_)+/
+		) != nil)
+
+		#expect(simplifyPath(path) == expected)
+	}
 }
