e6c28e6ba9014e884ca3ceecb76f480f836cc830
Chengjie Mao <minnows.lodge_0s@icloud.com>
2024-08-03 03:36:53 -0400

    Add "Min stack"

--- Tests/LeetCodeTests/StackTests.swift
+++ Tests/LeetCodeTests/StackTests.swift
@@ -34,4 +34,47 @@ struct StackTests {
 
 		#expect(simplifyPath(path) == expected)
 	}
+
+	@Test("Min stack", arguments: [
+		TestCase(
+			given: [
+				.initialize, .push(-2), .push(0), .push(-3),
+				.getMin, .pop, .top, .getMin,
+			] as [MinStackOperation],
+			expected: [
+				nil, nil, nil, nil,
+				-3, nil, 0, -2
+			]
+		)
+	])
+	func testMinStack(c: TestCase<[MinStackOperation], [Int?]>) throws {
+		let (operations, expected) = (c.given, c.expected)
+
+		try #require(operations.count == expected.count)
+
+		let stack = MinStack()
+
+		for (operation, expected) in zip(operations, expected) {
+			switch operation {
+				case .initialize:
+					break
+				case .push(let num):
+					stack.push(num)
+				case .pop:
+					stack.pop()
+				case .getMin:
+					#expect(stack.getMin() == expected)
+				case .top:
+					#expect(stack.top() == expected)
+			}
+		}
+	}
+}
+
+enum MinStackOperation: Encodable {
+	case initialize
+	case push(Int)
+	case pop
+	case getMin
+	case top
 }
