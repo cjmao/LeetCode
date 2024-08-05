5473ee5eb07a04849fea73ab00350962963764b0
Chengjie Mao <minnows.lodge_0s@icloud.com>
2024-08-05 13:56:59 -0400

    Complete "Merge two sorted lists"

--- Sources/LeetCode/LinkedList.swift
+++ Sources/LeetCode/LinkedList.swift
@@ -175,5 +175,27 @@ func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
 ///
 /// Return _the head of the merged linked list_.
 func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
-	nil
+	var head: ListNode?, tail: ListNode?
+	var l1 = list1, l2 = list2
+
+	while l1 != nil || l2 != nil {
+		let node: ListNode?
+
+		if l2 == nil || l1 != nil && l1!.val <= l2!.val {
+			node = l1
+			l1 = l1?.next
+		} else {
+			node = l2
+			l2 = l2?.next
+		}
+
+		if head == nil {
+			head = node
+		}
+
+		tail?.next = node
+		tail = node
+	}
+
+	return head
 }
