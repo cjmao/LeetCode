2c2bcfe8568697a7374d443e118328f3beefde4d
Chengjie Mao <minnows.lodge_0s@icloud.com>
2024-09-19 23:26:34 -0400

    Solve "Convert sorted array to binary search tree"

--- Sources/LeetCode/DivideAndConquer.swift
+++ Sources/LeetCode/DivideAndConquer.swift
@@ -3,5 +3,17 @@
 /// Given an integer array `nums` where the elements are sorted in **ascending
 /// order**, convert _it to a **height-balanced** binary search tree_.
 func sortedArrayToBST(_ nums: [Int]) -> TreeNode? {
-	nil
+	func convert(from start: Int, to end: Int) -> TreeNode? {
+		guard start <= end else { return nil }
+
+		let midpoint = (start + end + 1) / 2
+		let root = TreeNode(nums[midpoint])
+
+		root.left = convert(from: start, to: midpoint - 1)
+		root.right = convert(from: midpoint + 1, to: end)
+
+		return root
+	}
+
+	return convert(from: nums.startIndex, to: nums.endIndex - 1)
 }
